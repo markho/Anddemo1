e156e99f22283f0f7ea712a67b0a2071581b795b
mark <mark@patristar.com>
2012-08-08 14:45:34 +0800

    second commit

--- myapp1/src/com/example/myapp1/MainActivity.java
+++ myapp1/src/com/example/myapp1/MainActivity.java
@@ -6,13 +6,14 @@ import android.view.Menu;
 
 
 public class MainActivity extends Activity {
-
+	// add more comments
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    // add comment
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
